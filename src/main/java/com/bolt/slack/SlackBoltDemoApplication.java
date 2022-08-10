368c20ea7e407802a9f9f697750f8ff84644b35a
riju <rijuprakash03@gmail.com>
2022-08-10 20:37:41 +0530

    Initial commit

---
+++ src/main/java/com/bolt/slack/SlackBoltDemoApplication.java
@@ -0,0 +1,13 @@
+package com.bolt.slack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlackBoltDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlackBoltDemoApplication.class, args);
+	}
+
+}
