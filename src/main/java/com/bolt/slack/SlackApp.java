21546491fcb740763c28ef9d5d26d9dcaccd4fbc
riju <rijuprakash03@gmail.com>
2022-08-11 11:51:19 +0530

    Implementing block kit

--- src/main/java/com/bolt/slack/SlackApp.java
+++ src/main/java/com/bolt/slack/SlackApp.java
@@ -1,8 +1,6 @@
 package com.bolt.slack;
 
 import com.slack.api.bolt.App;
-import com.slack.api.bolt.AppConfig;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import static com.slack.api.model.block.Blocks.*;
@@ -11,44 +9,10 @@ import static com.slack.api.model.block.element.BlockElements.*;
 
 
 @Configuration
-@Slf4j
 public class SlackApp {
-
-  // export SLACK_BOT_TOKEN=xoxb-3906576084562-3899973152886-3sRQ46mLCCx7jBWabDAKifbt
-  //  export SLACK_SIGNING_SECRET=3bd4b8abdfb87dfec9d43873f55cb121
-
-    // If you would like to run this app for a single workspace,
-    // enabling this Bean factory should work for you.
-     @Bean
-    public AppConfig loadSingleWorkspaceAppConfig() {
-        return AppConfig.builder()
-                .singleTeamBotToken(System.getenv("SLACK_BOT_TOKEN"))
-                .signingSecret(System.getenv("SLACK_SIGNING_SECRET"))
-                .build();
-
-    }
-
-    // If you would like to run this app for multiple workspaces,
-    // enabling this Bean factory should work for you.
-    // @Bean
-    public AppConfig loadOAuthConfig() {
-        return AppConfig.builder()
-                .singleTeamBotToken(null)
-                .clientId(System.getenv("SLACK_CLIENT_ID"))
-                .clientSecret(System.getenv("SLACK_CLIENT_SECRET"))
-                .signingSecret(System.getenv("SLACK_SIGNING_SECRET"))
-                .scope("app_mentions:read,channels:history,channels:read,chat:write")
-                .oauthInstallPath("/slack/install")
-                .oauthRedirectUriPath("/slack/oauth_redirect")
-                .build();
-    }
-
     @Bean
-    public App initSlackApp(AppConfig config) {
-        App app = new App(config);
-        if (config.getClientId() != null) {
-            app.asOAuthApp(true);
-        }
+    public App initSlackApp() {
+        App app = new App();
         app.command("/hello", (req, ctx) -> {
             return ctx.ack(asBlocks(
                     section(section -> section.text(markdownText(":wave: pong"))),
@@ -59,7 +23,9 @@ public class SlackApp {
                     )
             ));
         });
+
         return app;
     }
-
 }
+
+
