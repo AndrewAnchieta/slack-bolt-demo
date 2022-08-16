13788696e4a0af4cd5aea2d564ab0830f64629b0
riju <rijuprakash03@gmail.com>
2022-08-16 17:10:02 +0530

    bot steps added

--- src/main/java/com/bolt/slack/SlackApp.java
+++ src/main/java/com/bolt/slack/SlackApp.java
@@ -42,9 +42,9 @@ public class SlackApp {
                        section(section -> section.text(markdownText("What do you want to know about Card Service SOAP. Please select from below options" + "\n"))),
                        actions(actions -> actions
                                .elements(asElements(
+                                       button(b -> b.actionId("Endpoint URL SOAP").text(plainText(pt -> pt.text("Endpoint URL"))).value("https://endpointurl.com" + "\n" + "https://ngi.com")),
+                                       button(b -> b.actionId("Jenkins Job SOAP").text(plainText(pt -> pt.text("jenkins"))).value("http://jenkinsjob.com" + "\n" + "http://jenkins2.com")),
                                        button(b -> b.actionId("Github URL SOAP").text(plainText(pt -> pt.text("Github URL"))).value("https://github.com" + "\n" + "http://memberservice.com" + "\n" + "http://cardservice.com")),
-                                       button(b -> b.actionId("Endpoint URL SOAP").text(plainText(pt -> pt.text("Endpoint URL"))).value("soap")),
-                                       button(b -> b.actionId("Jenkins Job SOAP").text(plainText(pt -> pt.text("jenkins"))).value("rest")),
                                        button(b -> b.actionId("Confluence link SOAP").text(plainText(pt -> pt.text("confluence link"))).value("rest")),
                                        button(b -> b.actionId("ECP Project details SOAP").text(plainText(pt -> pt.text("ecp project details"))).value("rest")),
                                        button(b -> b.actionId("KT Recordings SOAP").text(plainText(pt -> pt.text("KT Recording"))).value("rest")),
@@ -65,12 +65,33 @@ public class SlackApp {
                        actions(actions -> actions
                                .elements(asElements(
                                        button(b -> b.actionId("Github URL REST").text(plainText(pt -> pt.text("Github URL"))).value("http://jenkins.com" + "\n" + "http://memberservice.com" + "\n" + "http://cardservice.com")),
-                                       button(b -> b.actionId("Endpoint URL REST").text(plainText(pt -> pt.text("Endpoint URL"))).value("soap")),
-                                       button(b -> b.actionId("Jenkins Job SOAP REST").text(plainText(pt -> pt.text("jenkins"))).value("rest")),
+                                       button(b -> b.actionId("Endpoint URL REST").text(plainText(pt -> pt.text("Endpoint URL"))).value("https://endpointurl.com")),
+                                       button(b -> b.actionId("Jenkins Job SOAP REST").text(plainText(pt -> pt.text("jenkins"))).value("http://jenkinsjob.com")),
                                        button(b -> b.actionId("Confluence link REST").text(plainText(pt -> pt.text("confluence link"))).value("rest")),
                                        button(b -> b.actionId("ECP Project details REST").text(plainText(pt -> pt.text("ecp project details"))).value("rest")),
                                        button(b -> b.actionId("KT Recordings REST").text(plainText(pt -> pt.text("KT Recording"))).value("rest")),
                                        button(b -> b.actionId("Swagger Links REST").text(plainText(pt -> pt.text("Swagger Links"))).value("rest"))
+                               ))
+
+                       )
+               ));
+           }
+           return ctx.ack();
+       });
+
+
+
+       app.blockAction("Endpoint URL SOAP", (req, ctx) -> {
+           String value = req.getPayload().getActions().get(0).getValue();
+           if (req.getPayload().getResponseUrl() != null) {
+               ctx.respond("Please find the endpoint URL for Card Service SOAP" + "\n" + value);
+               ctx.respond(asBlocks(
+                       section(section -> section.text(markdownText("Do you want to know anything else about this service" + "\n"))),
+                       actions(actions -> actions
+                               .elements(asElements(
+                                       button(b -> b.actionId("YES").text(plainText(pt -> pt.text("YES"))).value("YES")),
+                                       button(b -> b.actionId("NO").text(plainText(pt -> pt.text("NO"))).value("NO"))
+
                                ))
 
                        )
@@ -98,6 +119,27 @@ public class SlackApp {
            return ctx.ack();
        });
 
+       app.blockAction("Jenkins Job SOAP", (req, ctx) -> {
+           String value = req.getPayload().getActions().get(0).getValue();
+           if (req.getPayload().getResponseUrl() != null) {
+               ctx.respond("Please find the below Jenkins Job for Card Service SOAP"+ "\n" + value);
+               ctx.respond(asBlocks(
+                       section(section -> section.text(markdownText("Do you want to know anything else about this service" + "\n"))),
+                       actions(actions -> actions
+                               .elements(asElements(
+                                       button(b -> b.actionId("YES").text(plainText(pt -> pt.text("YES"))).value("YES")),
+                                       button(b -> b.actionId("NO").text(plainText(pt -> pt.text("NO"))).value("NO"))
+
+                               ))
+
+                       )
+               ));
+           }
+           return ctx.ack();
+       });
+
+
+
        app.blockAction("NO", (req, ctx) -> {
            String value = req.getPayload().getActions().get(0).getValue();
            if (req.getPayload().getResponseUrl() != null) {
@@ -113,9 +155,9 @@ public class SlackApp {
                        section(section -> section.text(markdownText("Click on below to know more !!" + "\n"))),
                        actions(actions -> actions
                                .elements(asElements(
+                                       button(b -> b.actionId("Endpoint URL SOAP").text(plainText(pt -> pt.text("Endpoint URL"))).value("https://endpointurl.com" + "\n" + "https://ngi.com")),
+                                       button(b -> b.actionId("Jenkins Job SOAP").text(plainText(pt -> pt.text("jenkins"))).value("http://jenkinsjob.com" + "\n" + "http://jenkins2.com")),
                                        button(b -> b.actionId("Github URL SOAP").text(plainText(pt -> pt.text("Github URL"))).value("https://github.com" + "\n" + "http://memberservice.com" + "\n" + "http://cardservice.com")),
-                                       button(b -> b.actionId("Endpoint URL SOAP").text(plainText(pt -> pt.text("Endpoint URL"))).value("soap")),
-                                       button(b -> b.actionId("Jenkins Job SOAP").text(plainText(pt -> pt.text("jenkins"))).value("rest")),
                                        button(b -> b.actionId("Confluence link SOAP").text(plainText(pt -> pt.text("confluence link"))).value("rest")),
                                        button(b -> b.actionId("ECP Project details SOAP").text(plainText(pt -> pt.text("ecp project details"))).value("rest")),
                                        button(b -> b.actionId("KT Recordings SOAP").text(plainText(pt -> pt.text("KT Recording"))).value("rest")),
@@ -129,9 +171,7 @@ public class SlackApp {
        });
 
 
-
-
-        app.event(ReactionAddedEvent.class, (payload, ctx) -> {
+       app.event(ReactionAddedEvent.class, (payload, ctx) -> {
             ReactionAddedEvent event = payload.getEvent();
             if (event.getReaction().equals("white_check_mark")) {
                 ChatPostMessageResponse message = ctx.client().chatPostMessage(r -> r
